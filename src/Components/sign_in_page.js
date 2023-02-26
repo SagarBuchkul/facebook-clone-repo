8e3b957b3979a1846377034f0cfe3777e6982146
sagar-buchkul <buchkulakash40@gmail.com>
2023-02-26 21:54:53 +0530

    updated sub_branch

--- src/Components/sign_in_page.js
+++ src/Components/sign_in_page.js
@@ -2,9 +2,14 @@
 function Sign_in_page() {
   return (
 
-        <h1 className="text-3xl font-bold underline text-blue-600">
+        <div>
+          <h1 className="text-3xl font-bold underline text-blue-600">
            Sign in page of facebook
         </h1>
+        <h4 className="text-3xl font-bold underline text-blue-600">
+          This change is added on sub_branch
+         </h4>
+        </div>
   );
 }
 
