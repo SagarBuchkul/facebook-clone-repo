cfb9226dfd21f2f7cfd33107dcc32a09044b48c2
sagar-buchkul <buchkulakash40@gmail.com>
2023-02-26 21:34:33 +0530

    Updated sign_in_page text.

--- src/Components/sign_in_page.js
+++ src/Components/sign_in_page.js
@@ -3,7 +3,7 @@ function Sign_in_page() {
   return (
 
         <h1 className="text-3xl font-bold underline text-blue-600">
-           Sign in page
+           Sign in page of facebook
         </h1>
   );
 }
