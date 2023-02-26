2c7dfd5b1261be893f81332006e2f2cbb0c98980
sagar-buchkul <buchkulakash40@gmail.com>
2023-02-26 22:07:56 +0530

    Login.js added to sub_branch

---
+++ src/Components/Login.js
@@ -0,0 +1,15 @@
+
+function Login() {
+    return (
+  
+          <div>
+                <h1 className="text-3xl font-bold underline text-red-600">
+                  Login page of facebook
+               </h1>
+          
+          </div>
+    );
+  }
+  
+  export default Login;
+  
